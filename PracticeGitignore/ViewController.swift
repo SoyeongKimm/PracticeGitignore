0d7a9b983ac8a4c21ccb41bea675f2c07a414584
SoyeongKimm <ternintoswift@gmail.com>
2023-11-30 12:05:01 +0900

    Create file

---
+++ PracticeGitignore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeGitignore
+//
+//  Created by Hyeonho So on 2023/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
